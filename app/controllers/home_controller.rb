4b0583bc304533f0141c90f31597d542aa4b51be
Joseph Leon <joseph.leon.9@gmail.com>
2016-10-13 09:27:18 -0400

    Write code that iterates through 1..1000 and pushes all values divisible by 3 and 5 into an array.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
+
+  	@multiples_of_3_5 = Array.new
+  	for num in 1..1000 do
+  		if (num % 3) == 0 || (num % 5) == 0
+  			@multiples_of_3_5.push(num)
+  		end
+  	end
+
   end
 end
